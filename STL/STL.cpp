fc8c87b6bf033b63ebe974eaaf260180caca688f
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-06-16 23:21:59 +0530

    Vector
    Decleration of vector (ProtoType):                                  
                                                       Vector < Data_type > Vector_Name ;
     re-declaration of the variable v.
    In C++, you cannot declare the same variable twice in the same scope.

--- STL/STL.cpp
+++ STL/STL.cpp
@@ -4,7 +4,20 @@
 
 using namespace std;
 int main(){
+/*
+                        Declare Vector                 
+  Vector<Data_type> Vector_Name ;
 
+   The issue in the code you provided lies in the re-declaration of the variable v.
+   In C++, you cannot declare the same variable twice in the same scope.
+ 
+ vector <int> v;
+vector <int> v(4);
+   cout << "Size of V :" << v.size() << endl;
+   vector <int> v(4, 2);
+
+
+*/
 
 
 
