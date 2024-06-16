c66450342d390874478e12d79d481e0a68f24d62
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-06-16 23:25:57 +0530

    Size and Print The element of the vector
    vector <int> v2(4); ---> vector <int> v2(size,value)

--- STL/STL.cpp
+++ STL/STL.cpp
@@ -18,7 +18,13 @@ vector <int> v(4);
 
 
 */
-
+  vector <int> v1;
+  vector <int> v2(4);
+     cout << "Size of V :" << v2.size() << endl;
+     vector <int> v3(4, 2);  //    vector <int> v3 (size,value) ;
+     for (int i = 0; i < v3.size(); i++) {
+         std::cout << v3[i] << " ";
+     }
 
 
 
