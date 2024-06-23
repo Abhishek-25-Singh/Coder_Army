3f0b8ba3f7bddf6903d74f1843869c32e33b1d28
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-06-23 13:32:16 +0530

    Access the Elements of an Array
    INteger TYpe Data TYpe Behavioure

--- 16Lecture(Array)/Array.cpp
+++ 16Lecture(Array)/Array.cpp
@@ -9,5 +9,12 @@ std::cout << cars[1] << std::endl;
 std::cout << cars[2] << std::endl;
 std::cout << cars[3] << std::endl;
 //std::cout << cars[3] << std::endl;
+int arr1[] = { 11,22,33,44,55,66 };
+std::cout << arr1[0] << std::endl;
+std::cout << arr1[1] << std::endl;
+std::cout << arr1[2] << std::endl;
+std::cout << arr1[3] << std::endl;
+std::cout << arr1[4] << std::endl;
+std::cout << arr1[5] << std::endl;
 	return 0;
 }
