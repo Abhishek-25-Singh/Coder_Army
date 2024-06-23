4efe9255cfbd200df4eedda21d692e85f0bd4b34
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-06-23 13:49:52 +0530

    Array Initialization
    🔹it's possible to initialize an array during declaration
      ➜Example :
      ➜Code :
       // declare and initialize an array
           int arr1[5] = { 6 , 8 , 5 , 1 , 9 };
    ────────────────────────────────────────
    🔹we have not mentioned the size of the array. 
    🔹In such cases, the compiler automatically computes the size.
             ➜Example :
             ➜Code :
                 // declare and initialize an array
                     int arr2[] = { 3 , 8  ,2 ,  9 };
    ────────────────────────────────────────
    🔹if an array has a size n, we can store up to n number of 
         elements in the array. 
    🔹However, what will happen if we store less than n number of 
         elements.
             ➜Example :
             ➜Code :
                 // store only 3 elements in the array
                          int arr3[6] = {19, 10, 8};
    🔹Here, the array (arr3) has a size of 6. However, we have 
         initialized it with only 3 elements.
    🔹In such cases, the compiler assigns random values to the 
         remaining places. Often, this random value is simply 0.

--- 16Lecture(Array)/Array.cpp
+++ 16Lecture(Array)/Array.cpp
@@ -16,5 +16,10 @@ std::cout << arr1[2] << std::endl;
 std::cout << arr1[3] << std::endl;
 std::cout << arr1[4] << std::endl;
 std::cout << arr1[5] << std::endl;
+
+	//Array Initialization
+int arr1[5] = { 6,8,5,1,9 };   // Name of the Array is arr and size is 5
+int arr2[] = { 3,8,2,  9  };     //--> Without declaring the size 
+int arr3[6] = { 19, 10, 8 }; //---> Remaining value will be zero
 	return 0;
 }
