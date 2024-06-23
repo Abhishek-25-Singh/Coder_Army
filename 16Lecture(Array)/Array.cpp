ab68966d7e182b76d5cb5c1dc7faa5b306c26dcd
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-06-23 13:13:39 +0530

    Array Decleration
                                             👉 Array 👈
    📍Arrays are used to store multiple values in a single variable, 
         instead of declaring separate variables for each value.
    📍In C++, an array is a variable that can store multiple values of the 
         same type.
    📍In C++, the size and type of arrays cannot be changed after 
         its declaration.
    ✦ Example ;
              🔹 int arr[100];
             🔹 arr is an array that can hold a maximum of 100 
                   elements of integer type.

---
+++ 16Lecture(Array)/Array.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+// Array Declaration 
+int arr[100]; //arr-> Name of the array and 100 is Size of that array
+std::string  cars[] = { "Volvo", "BMW", "Ford", "Mazda" };
+
+	return 0;
+}
