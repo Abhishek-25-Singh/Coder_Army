ecf28afb1e5d23eb29e47b69593910cca6f9568a
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-06-23 13:26:23 +0530

    Access the Elements of an Array
    All the statement accesses the value of the elements in cars

--- 16Lecture(Array)/Array.cpp
+++ 16Lecture(Array)/Array.cpp
@@ -4,6 +4,9 @@ int main()
 // Array Declaration 
 int arr[100]; //arr-> Name of the array and 100 is Size of that array
 std::string  cars[] = { "Volvo", "BMW", "Ford", "Mazda" };
-
+std::cout << cars[0] << std::endl;
+std::cout << cars[1] << std::endl;
+std::cout << cars[2] << std::endl;
+std::cout << cars[3] << std::endl;
 	return 0;
 }
